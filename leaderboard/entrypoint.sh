4112490f15f242f0205c9fb704219ed728882cb2
bobbyskywalker <aleksander.garbacz@gmail.com>
2025-09-20 17:22:52 +0200

    leaderboard code moved to new repo

--- leaderboard/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -e
-
-python /app/src/db_create.py
-
-exec python /app/src/app.py
--- leaderboard/src/queries.py
+++
@@ -1,2 +0,0 @@
-get_highest_scores = "SELECT * FROM scores ORDER BY score DESC LIMIT 5;"
-insert_score = "INSERT INTO scores (name, score) VALUES (?, ?);"
